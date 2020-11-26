47e0bf8f5e1b6d8c4adf6e43ff8c6461b55c50ae
Francisco Morales <fcomorales.sanchez@gmail.com>
2020-11-26 16:37:59 -0300

    Modularización, plantilla AdminLTE

---
+++ vistas/modulos/cabecera.php
@@ -0,0 +1,51 @@
+<header class="main-header">
+    <!-- ===============================================
+    =                   LOGO                   =
+    =============================================== -->
+    <a href="" class="logo">
+
+<!--        logo mini-->
+        <span class="logo-mini">
+            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive"
+             style="padding: 10px">
+        </span>
+<!--        logo normal-->
+        <span class="logo-lg">
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive"
+                 style="padding: 10px 0px">
+        </span>
+
+    </a>
+
+    <!-- ===============================================
+        =             BARRA NAVEGACION                   =
+        =============================================== -->
+        <nav class="navbar navbar-static-top" role="navigation">
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only">Toggle nav</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="vistas/img/usuarios/default/anonymous.png" class="user-image">
+                            <span class="hidden-xs">Usuario admin</span>
+                        </a>
+                        <!-- Dropdown-toggle -->
+                        <ul class="dropdown-menu">
+                            <li class="user-body">
+                                <a href="#" class="btn btn-default btn-flat">Salir</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+
+        </nav>
+
+
+</header>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2015-2020 <a href="https://adminlte.io">Inventory System</a>.</strong>
+    Totos los derechos reservados
+
+</footer>
