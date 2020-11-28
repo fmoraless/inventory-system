3a9306a598396fd5f29f42038b9bd9750bf91680
Francisco Morales <fcomorales.sanchez@gmail.com>
2020-11-28 03:06:04 -0300

    Controller y modelo login usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -2,8 +2,37 @@
 
 class ControladorUsuarios
 {
-    public function ctrPlantilla()
+    /*================================
+    INGRESO USUARIO
+    =================================*/
+    public function ctrIngresoUsuario()
     {
-        include "vistas/plantilla.php";
+        if (isset($_POST["ingUsuario"])){
+            if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
+
+                $tabla = "usuarios";
+
+                $item = "usuario";
+                $valor = $_POST["ingUsuario"];
+
+                $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+                if($respuesta["usuario"] == $_POST["ingUsuario"] && $respuesta["password"] == $_POST["ingPassword"])
+                {
+//                    echo '<br><div class="alert alert-success">Correcto</div>';
+                    $_SESSION["iniciarSesion"] = "ok";
+                    echo '<script>
+                            window.location = "inicio";
+                          </script>';
+                }else{
+                    echo '<br><div class="alert alert-danger">Fallido</div>';
+                }
+
+
+
+
+
+            }
+        }
     }
 }
--- modelos/usuarios.modelo.php
+++ modelos/usuarios.modelo.php
@@ -1,2 +1,24 @@
 <?php
+require_once "conexion.php";
 
+Class ModeloUsuarios{
+    /* =======================
+    MOSTRAR USUARIOS
+    =========================*/
+
+    public static function MdlMostrarUsuarios($tabla, $item, $valor){
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+
+        $stmt-> bindParam(":".$item, $valor, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch();
+
+
+
+
+
+
+
+    }
+}
