16e515355cd8f585a5e3eb90169147ce7d31c51b
Francisco Morales <fcomorales.sanchez@gmail.com>
2020-12-03 19:05:05 -0300

    Metodo crear usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -5,7 +5,7 @@ class ControladorUsuarios
     /*================================
     INGRESO USUARIO
     =================================*/
-    public function ctrIngresoUsuario()
+    static public function ctrIngresoUsuario()
     {
         if (isset($_POST["ingUsuario"])){
             if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
@@ -32,6 +32,62 @@ class ControladorUsuarios
 
 
 
+            }
+        }
+    }
+
+    /*================================
+        CREAR USUARIO
+    =================================*/
+    static public function ctrCrearUsuario()
+    {
+        if (isset($_POST["nuevoUsuario"])){
+
+            if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/',$_POST["nuevoNombre"]) &&
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoUsuario"]) &&
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"])){
+
+                $tabla = "usuarios";
+
+                $datos = array(
+                    "nombre" => $_POST["nuevoNombre"],
+                    "usuario" => $_POST["nuevoUsuario"],
+                    "password" => $_POST["nuevoPassword"],
+                    "perfil" => $_POST["nuevoPerfil"]
+                );
+
+                $respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla, $datos);
+
+                if ($respuesta == "ok"){
+                    echo '<script>
+                        Swal.fire({
+                            icon: "success",
+                            title: "¡El usuario ha sido guardado correctamente",
+                            showConfirmButton: true,
+                            confirmButtonText: "Cerrar",
+                            closeOnConfirm: false
+                        }).then((result){
+                            if(result.value){
+                                window.location = "usuarios";
+                            }
+                        });
+                      </script>';
+                }
+
+            }else{
+                echo '<script>
+                        Swal.fire({
+                            icon: "error",
+                            title: "¡El usuario no puede ir vacío ni ingresar caracteres especiales.!",
+                            showConfirmButton: true,
+                            confirmButtonText: "Cerrar",
+                            closeOnConfirm: false
+                        }).then((result){
+                            if(result.value){
+                                window.location = "usuarios";
+                            }
+                        });
+                      </script>';
             }
         }
     }
